e7138c67471f784e82dc8863ce0a2cdb47b32829
Sofiane <55191416+Yzar-brr@users.noreply.github.com>
2025-02-24 16:05:41 +0100

    Add files via upload

---
+++ src/data/user_info.js
@@ -0,0 +1,189 @@
+const info = {
+    // ============ MAIN DETAILS ============
+    main: {
+        name: "Sofiane Wibaut",
+        description: "Je suis un jeune développeur spécialisé en développement web. Je crée des applications avec Laravel, Alpine.js, Tailwind CSS et Livewire. J'aime résoudre des problèmes complexes et apprendre de nouvelles compétences.",
+        role: "Laravel Developer",
+        photo: "../photo.webp",
+        email: "sofiane.wibaut@gmail.com",
+    },
+
+    // ============ SOCIAL LINKS ============
+    socials: {
+		github: "https://github.com/yzar-brr",
+		linkedin: "https://linkedin.com/in/sofiane-wibaut",
+    },
+
+    // ============ PROJECTS ============
+    projects: [
+        {
+			title: "goodToCook",
+			description: "Application web de recette développé avec Laravel",
+            technologies: "Laravel, Livewire, Tailwind, AlpineJS, MySQL",
+			github: "https://github.com/Yzar-brr/goodToCook",
+            link: "#"
+		},
+
+        {
+			title: "Préfacturation",
+			description: "Application TALL Stack de préfacturation",
+			technologies: "Tall Stack, Vanilla JS",
+			github: "#",
+            link: "#"
+		},
+
+        {
+			title: "MyCellar",
+			description: "Création de nouvlle fonctionnalité pour l'application MyCellar (Hillebrand Gori)",
+			technologies: "Laravel,Bootstrap4",
+			github: "#",
+            link: "https://mycellar.hillebrandgori.com/login"
+		},
+
+		// {
+		// 	title: "Chatroom",
+		// 	description: "Chatroom is a simple online chatting web application where you can create chat rooms and have a fun conversation with a group of people.",
+		// 	technologies: "React, Tailwind, Firebase",
+		// 	github: "https://github.com/dsbalico/chatroom",
+        //     link: "https://thechatroom.netlify.app/"
+		// },
+
+		// {
+		// 	title: "Sudoku Solver",
+		// 	description: "Sudoku Solver is a web application that can generate or solve sudoku problems using simulated annealing, an optimization algorithm.",
+		// 	technologies: "React, Tailwind, TypeScript",
+		// 	github: "https://github.com/dsbalico/sudoku-solver-using-simulated-annealing",
+        //     link: "https://sudoku-solver-sa.netlify.app/"
+		// },
+
+		// {
+		// 	title: "Probability Pick",
+		// 	description: "Probability Pick is a PCSO Lotto Number Generator that calculates the likelihood of selecting winning numbers based on historical draw data.",
+		// 	technologies: "React, Tailwind, Flask",
+		// 	github: "https://github.com/dsbalico/pcso-lotto-number-generator",
+        //     link: "https://probability-pick.netlify.app/"
+		// },
+    ],
+
+    // ============ EDUCATION ============
+    education: [
+        {
+            school: "Dijon Formation | Dijon",
+            degree: "BTS SIO Option SLAM",
+            duration: "2023 - 2025",
+            image: "sti.webp"
+        },
+        {
+            school: "Lycée Henri Becquerel | Tours",
+            degree: "BAC Profesionnel Systèmes Numériques Option C",
+            duration: "2018 - 2021",
+            image: "tup.webp"
+        },
+    ],
+
+    // ============ EXPERIENCE ============
+    experience: [
+        {
+            position: "Alternane Développeur Web | Tall Stack",
+            company: "Hillebrand Gori France, a company of DHL.",
+            duration: "Sep 2023 - Aug 2025 (2 ans)",
+            image: "ccci.webp",
+            descriptions: [
+                "Suivi d'une formation approfondie en développement Laravel.",
+                "Contribution activement au développement continu d'une application web.",
+                "Encadré par un développeur senior pour l'apprentissage et l'optimisation du code",
+            ]
+        },
+        {
+            position: "Stagiare Big Data | Excel Data",
+            company: "Nestlé Notre Dame D'Oé.",
+            duration: "Dec 2022 - Feb 2023 (3 Mois)",
+            image: "spvttc.webp",
+            descriptions: [
+                "Gestion de données agronomiques pour l'analyse de la production.",
+                "Aide au développement d'un outil de gestion de données.",
+                "Formation à l'analyse de données et à la visualisation de données.",
+            ]
+        },
+        {
+            position: "Stagiare Réseaux | AD",
+            company: "IUT Tours Nord.",
+            duration: "Jan 2022 - Feb 2022 (1 Mois)",
+            image: "spvttc.webp",
+            descriptions: [
+                "Configuration de routeurs et de commutateurs.",
+                "Création d'un AD pour la gestion des utilisateurs.",
+
+            ]
+        },
+    ],
+
+    // ============ CERTIFICATES ============
+    certificates: [
+        {
+            title: "Foundations of Cybersecurity Certificate",
+            description: "Provided by Coursera & Authorized by IBM",
+            icon: "ibm",
+            link: "https://www.coursera.org/account/accomplishments/records/SF2PPLNVTH6R",
+        },
+        {
+            title: "Git and GitHub Essentials",
+            description: "Provided by Coursera & Authorized by IBM",
+            icon: "ibm",
+            link: "https://www.credly.com/badges/fa00026a-4db5-4269-ab4f-340f5382ac03",
+        },
+        {
+            title: "Crash Course on Python Certificate",
+            description: "Provided by Coursera & Authorized by Google",
+            icon: "google",
+            link: "https://www.coursera.org/account/accomplishments/verify/S9773NBEK4S6"
+        },
+        // {
+        //     title: "Technical Support Fundamentals Certificate",
+        //     description: "Provided by Coursera & Authorized by Google",
+        //     icon: "google",
+        //     link: "https://www.coursera.org/account/accomplishments/verify/P9EH5HNYRESZ"
+        // },
+        // {
+        //     title: "Introduction to Web Development with HTML, CSS, JavaScript Certificate",
+        //     description: "Provided by Coursera & Authorized by IBM",
+        //     icon: "ibm",
+        //     link: "https://www.credly.com/badges/7b88eb00-e1f9-4ae6-be2a-3b2ae8983c44"
+        // },
+        // {
+        //     title: "Introduction to Cloud Computing Certificate",
+        //     description: "Provided by Coursera & Authorized by IBM",
+        //     icon: "ibm",
+        //     link: "https://www.credly.com/badges/514b694a-8dc4-418d-9af4-61908dc29ca8"
+        // },
+        // {
+        //     title: "Django Application Development with SQL and Databases Certificate",
+        //     description: "Provided by Coursera & Authorized by IBM",
+        //     icon: "ibm",
+        //     link: "https://www.credly.com/badges/a36dd8a3-1050-4c4e-bcef-97b39babf55e"
+        // },
+        // {
+        //     title: "Front-End Developer (React) Certificate",
+        //     description: "Provided by HackerRank",
+        //     icon: "hackerrank",
+        //     link: "https://www.hackerrank.com/certificates/631ac3acf267"
+        // },
+        // {
+        //     title: "Software Engineer Certificate",
+        //     description: "Provided by HackerRank",
+        //     icon: "hackerrank",
+        //     link: "https://www.hackerrank.com/certificates/efdbdd44eb7d"
+        // }
+    ],
+
+    // ============ CONTACT ============
+    contact: {
+        title: "Prenons contact: Façons de me joindre",
+        description: "Merci de votre intérêt pour me contacter. Je suis ouvert à vos retours, questions et suggestions. Si vous avez une demande spécifique, n'hésitez pas à m'envoyer un e-mail directement à sofiane.wibaut@gmail.com. Je m'efforce de répondre à tous les messages sous 72h.",
+    },
+
+    // ============ FOOTER ============
+    footer: "© 2025 Sofiane Wibaut. All Rights Reserved"
+}
+
+export default info
